335e25f9983933e4e32825c199eff2f73fa6083d
The Force <2040992+TheForce172@users.noreply.github.com>
2025-03-08 12:28:25 +0000

    Add add links

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.businesscard.ui.theme.BusinessCardTheme
@@ -49,6 +53,7 @@ class MainActivity : ComponentActivity() {
 fun MainScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         CardCenter(modifier = Modifier.weight(1f))
+        CardBottom(modifier = Modifier.weight(0.25f))
     }
 }
 @Composable
@@ -79,6 +84,51 @@ fun CardCenter(modifier: Modifier = Modifier) {
         )
     }
 }
+
+@Composable
+fun CardBottom(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        CardItem(
+            icon = painterResource(R.drawable.github_mark),
+            iconDesc = "Github",
+            text= "https://github.com/TheForce172"
+            )
+        CardItem(
+            icon = painterResource(R.drawable.gmail_icon__2020_),
+            iconDesc = "Email",
+            text = "theforce172@gmail.com"
+        )
+        CardItem(
+            icon = painterResource(R.drawable.logo),
+            iconDesc = "X",
+            text = "@theforce172"
+        )
+    }
+
+}
+
+@Composable
+fun CardItem(icon: Painter, iconDesc: String, text: String, modifier: Modifier = Modifier){
+    Row(modifier = modifier.padding(8.dp).fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center){
+        Icon(
+            painter = icon,
+            contentDescription = iconDesc,
+            modifier = Modifier
+                .size(28.dp)
+
+        )
+        Text(
+            text = text,
+            fontSize = 20.sp,
+            modifier= Modifier.padding( start = 12.dp)
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
