ecd40df5ef7569432d3cecf7d4916618b52a0860
The Force <2040992+TheForce172@users.noreply.github.com>
2025-03-08 11:46:14 +0000

    Add central text

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,7 +24,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.businesscard.ui.theme.BusinessCardTheme
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,6 +65,17 @@ fun CardCenter(modifier: Modifier = Modifier) {
                 .size(128.dp)
                 .clip(CircleShape)
                 .border(2.dp, Color.Gray, CircleShape)
+        )
+        Text(
+            text = "Cranston Parker",
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Desperately seeking employment",
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center
         )
     }
 }
